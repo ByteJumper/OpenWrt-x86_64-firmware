8e558ca0996f29d45244d93e5eec441496df37b0
Andrew <40738794+ByteJumper@users.noreply.github.com>
2026-04-30 10:04:59 +0800

    Update diy-part1.sh
    增加 homeprox 和 passwall2

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,4 +15,5 @@
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git homeproxy https://github.com/immortalwrt/homeproxy.git' >>feeds.conf.default
+echo 'src-git openwrt-passwall2 https://github.com/Openwrt-Passwall/openwrt-passwall2.git' >>feeds.conf.default
